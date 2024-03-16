db45a0cd1d337296eafe776202d0db9163389102
Ursoo <ursu.calin18@gmail.com>
2024-03-17 00:09:46 +0200

    added random password generating functionality

--- password_generator.py
+++ password_generator.py
@@ -1,11 +1,13 @@
+from random import randrange
+
 class PasswordGenerator:
-    def __init__(self, pass_length = 8):
+    def __init__(self, pass_length = 12):
         self.password_length = pass_length
 
     def generate(self):
         password = ''
 
         while (len(password) < self.password_length):
-            password+= f'{len(password)}'
+            password+= chr(randrange(33,127))
 
         return password
