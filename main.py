3c873803d5bcc6b9fe8154eef14bb6bf4e8cc752
Ursoo <ursu.calin18@gmail.com>
2024-04-02 18:25:23 +0300

    restructured files and folders, improved password generation algorithm

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from password_generator import PasswordGenerator
+from password_generator.password_generator import PasswordGenerator
+from validation import validate_password
 
 password_generator = PasswordGenerator()
 
-password, check_result = password_generator.generate()
+check_result = True
 
-print(password, check_result)
+while check_result == True:
+    password = password_generator.generate()
+
+    check_result = validate_password(password)
+
+    print(password, check_result)
