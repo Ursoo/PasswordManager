dc67fa8b2f5a11386b8a63c97d4f6c0af36f7812
Ursoo <ursu.calin18@gmail.com>
2024-04-17 12:38:12 +0300

    gitignore was in another directory :)

--- main.py
+++ main.py
@@ -7,19 +7,6 @@ from password_file_controller.controller import PasswordController
 
 #user_controller.read_user_configuration()
 
-password_controller = PasswordController('resources/configuration/my_passwords.txt')
-
-#print(password_controller.read_passwords())
-print(password_controller.add_password_to_file('psldapxzcvz', 'google'))
-print(password_controller.add_password_to_file('psld1a,pxzcvZ', 'google'))
-
-password_controller2 = PasswordController('resources/configuration/my_passwords_empty.txt')
-
-print(password_controller2.read_passwords())
-
-password_controller3 = PasswordController('resources/configuration/asdasy.txt')
-
-print(password_controller3.read_passwords())
 
 #password_generator = PasswordGenerator()
 
